52d12a341fd09705d0cf2db4209f98513f0fe2da
Saksham Verma <sakshamverma1000@gmail.com>
2024-09-12 08:07:41 +0530

    Created the changing image effect on page 3

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+let elemContainer = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+
+elemContainer.addEventListener("mouseenter", () => {
+  fixedImage.style.display = "block";
+});
+elemContainer.addEventListener("mouseleave", () => {
+  fixedImage = document.querySelector("#fixed-image");
+  fixedImage.style.display = "none";
+});
+
+let elements = Array.from(document.querySelectorAll(".elem"));
+
+elements.forEach((e) => {
+  e.addEventListener("mouseenter", () => {
+    let src = e.getAttribute("data-image");
+    fixedImage.style.backgroundImage = `url(${src})`;
+  });
+});
